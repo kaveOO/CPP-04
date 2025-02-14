6924f3f89eada1ad7c1943e6899f93f2d4e96bf0
kaveO <alban.billiette@gmail.com>
2025-02-14 07:00:12 +0100

    + | Finished ex00, just have to make test classes

---
+++ ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 05:39:26 by albillie          #+#    #+#             */
+/*   Updated: 2025/02/14 06:56:30 by albillie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << std::endl;
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << meta->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	std::cout << std::endl;
+
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	return 0;
+}
