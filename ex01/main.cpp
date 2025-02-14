812bf2313878ebb4bc863c7a9bd3c18234459135
kaveOO <alban.billiette@gmail.com>
2025-02-14 16:48:39 +0100

    + | Updated Makefiles and added all files from 00 in 01

---
+++ ex01/main.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 05:39:26 by albillie          #+#    #+#             */
+/*   Updated: 2025/02/14 07:10:22 by albillie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << std::endl;
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << meta->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	std::cout << std::endl;
+
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+
+	return 0;
+}
+
+
+// int main()
+// {
+// 	const WrongAnimal* j = new WrongAnimal();
+// 	const WrongAnimal* i = new WrongCat();
+
+// 	std::cout << std::endl;
+// 	std::cout << j->getType() << " " << std::endl;
+// 	std::cout << i->getType() << " " << std::endl;
+// 	std::cout << std::endl;
+
+// 	i->makeSound();
+// 	j->makeSound();
+
+// 	return 0;
+// }
