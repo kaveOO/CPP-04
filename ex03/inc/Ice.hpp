474e90a0d53a490c9585129319356edc87fa8f64
kaveO <alban.billiette@gmail.com>
2025-02-16 04:22:56 +0100

    + | Added Ice class and fullfilled Ice.hpp

--- ex03/inc/Ice.hpp
+++ ex03/inc/Ice.hpp
@@ -1,39 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*   Ice.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/16 02:20:42 by albillie          #+#    #+#             */
-/*   Updated: 2025/02/16 04:21:59 by albillie         ###   ########.fr       */
+/*   Created: 2025/02/16 03:50:24 by albillie          #+#    #+#             */
+/*   Updated: 2025/02/16 04:19:12 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
+#include "AMateria.hpp"
 
-class ICharacter;
-
-class AMateria
+class Ice : public AMateria
 {
-	protected:
-		// Attribute(s)
-		std::string type;
 	public:
 		// Constructor(s)
-		AMateria();
-		AMateria(std::string const &type);
-		AMateria(const AMateria &src);
-		// Destructor(S)
-		~AMateria();
-		// Overload Operator(s)
-		AMateria &operator=(const AMateria &assign);
-		// Getter(S)
-		std::string const &getType() const;
-		// Other Function(s)
-		virtual AMateria *clone() const = 0;
-		virtual void use(ICharacter &target);
+		Ice();
+		Ice(const Ice &src);
+		// Destructor(s)
+		~Ice();
+		// Overloard Operator(s)
+		Ice &operator=(const Ice &assign);
+		AMateria *clone() const;
+		void use(ICharacter &target);
 };
-
--- ex03/src/AMateria.cpp
+++ ex03/src/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 02:20:26 by albillie          #+#    #+#             */
-/*   Updated: 2025/02/16 03:23:57 by albillie         ###   ########.fr       */
+/*   Updated: 2025/02/16 04:04:34 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,26 @@
 // Constructor(s)
 AMateria::AMateria()
 {
-	std::cout << "AMateria -> Default constructor has been called" << std::endl;
-	type = "Default";
 }
 
-AMateria::AMateria(std::string const &newType)
+AMateria::AMateria(std::string const &type) : type(type)
 {
-	std::cout << "AMateria -> String constructor has been called" << std::endl;
-	type = newType;
 }
 
 AMateria::AMateria(const AMateria &src)
 {
-	std::cout << "AMateria -> Copy constructor has been called" << std::endl;
-	type = src.type;
+	(void) src;
 }
 
 // Destructor(S)
 AMateria::~AMateria()
 {
-	std::cout << "AMateria -> Destructor has been called" << std::endl;
 }
 
 // Overload Operator(s)
 AMateria &AMateria::operator=(const AMateria &assign)
 {
-	std::cout << "AMateria -> Overload assignment operator has been called" << std::endl;
-	type = assign.type;
-	return (*this);
+	(void) assign;
 }
 
 // Getter(S)
@@ -54,5 +46,5 @@ std::string const &AMateria::getType() const
 // Other Function(s)
 void AMateria::use(ICharacter &target)
 {
-
+	(void) target;
 }
