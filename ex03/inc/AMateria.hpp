28d5b6f8c5a685ff99f21ccc64b6785ec2f94eeb
kaveO <alban.billiette@gmail.com>
2025-02-16 02:50:11 +0100

    + | Added AMateria class with her .cpp file, now sorting includes and sources files in a different folder

---
+++ ex03/inc/AMateria.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/16 02:20:42 by albillie          #+#    #+#             */
+/*   Updated: 2025/02/16 02:48:22 by albillie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class AMateria
+{
+	protected:
+		// Attribute(s)
+		std::string type;
+	public:
+		// Constructor(s)
+		AMateria();
+		AMateria(std::string const &type);
+		AMateria(const AMateria &src);
+		// Destructor(S)
+		~AMateria();
+		// Overload Operator(s)
+		AMateria &operator=(const AMateria &assign);
+		// Getter(S)
+		std::string const &getType() const;
+		// Other Function(s)
+		virtual AMateria *clone() const = 0;
+		virtual void use(ICharacter &target);
+};
+
