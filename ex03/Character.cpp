584f1abba5966c7422cf803f8473208989a8a808
kaveOO <alban.billiette@gmail.com>
2025-02-17 21:43:35 +0100

    + | Working on inventory management functions

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Character.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kave0 <kave0@student.42.fr>                +#+  +:+       +#+        */
+/*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 04:52:27 by kave0             #+#    #+#             */
-/*   Updated: 2025/02/17 04:59:49 by kave0            ###   ########.fr       */
+/*   Updated: 2025/02/17 21:31:39 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ Character::Character()
 	{
 		this->gear[i] = NULL;
 	}
+	std::cout << "Character created with default constructor" << std::endl;
 }
 
 Character::Character(std::string name)
@@ -29,6 +30,7 @@ Character::Character(std::string name)
 	{
 		this->gear[i] = NULL;
 	}
+	std::cout << "Character created with string constructor" << std::endl;
 }
 
 Character::Character(const Character &copy)
@@ -36,16 +38,62 @@ Character::Character(const Character &copy)
 	this->name = copy.name;
 	for (size_t i = 0; i < 4; i++)
 	{
-		this->gear[i] = copy.gear[i] // TODO add clone out here
+		if (copy.gear[i])
+		{
+			this->gear[i] = copy.gear[i];
+		}
 	}
+	std::cout << "Character has been copied" << std::endl;
 }
 
 // Destructor(s)
-
 Character::~Character()
 {
 	for (size_t i = 0; i < 4; i++)
 	{
-		delete gear[i];
+		if (this->gear[i])
+		{
+			delete this->gear[i];
+		}
+	}
+	std::cout << "Character has been destroyed" << std::endl;
+}
+
+// Overload Operator(s)
+Character &Character::operator=(const Character &assign)
+{
+	this->name = assign.name;
+	return (*this);
+}
+
+// Getter(s)
+std::string const &Character::getName() const
+{
+	return (this->name);
+}
+
+// Gear Functions
+
+void Character::equip(AMateria *m)
+{
+	for (size_t i = 0; i < 4; i++)
+	{
+	}
+}
+
+void Character::unequip(int idx)
+{
+	if (idx >= 0 && idx <= 4  && this->gear[idx])
+	{
+		delete this->gear[idx];
+		this->gear[idx] = NULL;
+	}
+	else if (idx < 0 && idx > 4)
+	{
+		std::cout << "Invalid index on the inventory" << std::endl;
+	}
+	else
+	{
+
 	}
 }
